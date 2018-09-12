ecca1379512bc11f13e0457b7823fc0092db6b57
gncll <geencay@gmail.com>
2018-09-12 09:48:21 +0300

    plot3

--- plot3.R
+++ plot3.R
@@ -27,4 +27,4 @@ lines(datetime, subMetering3, type="l", col="blue")
 
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
 
-dev.off( )
+dev.off()
